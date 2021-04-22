142d1b1085abf180d8bb89a5a184a302f525c365
Aneke Allen <anekeapj@gmail.com>
2021-04-22 15:14:30 -0300

    Finalizado a parte de atualização do historicos de notas

--- painel-professor/turma/atualizar-historico.php
+++ painel-professor/turma/atualizar-historico.php
@@ -1,6 +1,5 @@
 <?php 
 
-include_once ('../../config.php');
 
 
 $query = $pdo->query("SELECT * FROM tbturma where IdTurma = '$turma'");
@@ -8,9 +7,17 @@ $res1 = $query->fetchAll(PDO::FETCH_ASSOC);
 $id_serie = $res1[0]['IdSerie'];
 $id_periodo = $res1[0]['IdPeriodo'];
 
+
+
 $query_resp = $pdo->query("SELECT * FROM tbserie where IdSerie = '$id_serie' ");
 $res_resp = $query_resp->fetchAll(PDO::FETCH_ASSOC);                    
 $codigo_serie = $res_resp[0]['CodigoSerie'];
+$id_curso = $res_resp[0]['IdCurso'];
+
+$query_r3 = $pdo->query("SELECT * FROM tbcurso where IdCurso = '".$id_curso."' ");
+$res_r3 = $query_r3->fetchAll(PDO::FETCH_ASSOC);
+
+$nome_curso = $res_r3[0]['NomeCurso'];
 
 
 $query_resp = $pdo->query("SELECT * FROM tbperiodo where IdPeriodo = '$id_periodo' ");
@@ -40,34 +47,72 @@ if (count($res) == 0) {
 	$pdo->query("UPDATE tbhistorico SET ResultadoFinal = '$situacao_turma' where CodigoSerie = '$codigo_serie' and IdAluno = '$aluno'");
 }
 
+$query5 = $pdo->query("SELECT * FROM tbfasenota where IdPeriodo = '$id_periodo' and IdSerie = '$id_serie' and NumeroFase = 8");
+$res5 = $query5->fetchAll(PDO::FETCH_ASSOC);
+$id_fase = $res5[0]['IdFaseNota'];
+
+
 //Atualizar o historico de notas
 
 $query = $pdo->query("SELECT * FROM tbgradecurricular where IdSerie = '$id_serie' and IdPeriodo = '$id_periodo'");
-$res2 = $query->fetchAll(PDO::FETCH_ASSOC);
+$res22 = $query->fetchAll(PDO::FETCH_ASSOC);
 
-for ($i=0; $i < count($res2); $i++) { 
-	foreach ($res2[$i] as $key => $value) {
+
+
+for ($i=0; $i < count($res22); $i++) { 
+	foreach ($res22[$i] as $key => $value) {
 	}
-	$id_disciplina = $res2[$i]['IdDisciplina'];
+
+	
+	$id_disciplina = $res22[$i]['IdDisciplina'];
+
+	$query_r = $pdo->query("SELECT * FROM tbdisciplina where IdDisciplina = '$id_disciplina'");
+	$res_r = $query_r->fetchAll(PDO::FETCH_ASSOC);
+
+
+	if ($nome_curso == 'Ensino Fundamental') {
+		$carga_horaria_disciplina = $res_r[0]['CH_Fundamental1'];
+	}else{
+		$carga_horaria_disciplina = $res_r[0]['CH_Fundamental2'];
+	}
+
+	$query12 = $pdo->query("SELECT * FROM tbsituacaoalunodisciplina where IdTurma = '$turma' and IdAluno = '$aluno' and IdDisciplina = '$id_disciplina'");
+	$res33 = $query12->fetchAll(PDO::FETCH_ASSOC);
+
+	$situacao_disc = $res33[0]['SituacaoAtual'];
+
+	$query3 = $pdo->query("SELECT * FROM tbhistoriconotas where CodigoSerie = '$codigo_serie' and IdAluno = '$aluno' and IdDisciplina = '$id_disciplina' and AnoConclusao = '$ano'");
+	$res_ver = $query3->fetchAll(PDO::FETCH_ASSOC);
 
 	
 
-	$res = $pdo->prepare("INSERT INTO tbgradecurricular SET IdSerie = :idserie, IdDisciplina = :id_disciplina, IdPeriodo = :idperiodo");
-
-	$res->bindValue(":idserie", $id_serie);
-	$res->bindValue(":id_disciplina", $id_disciplina);
-	$res->bindValue(":idperiodo", $id_ano);
+	
+	$query2 = $pdo->query("SELECT * FROM tbfasenotaaluno where IdTurma = '$turma' and IdAluno = '$aluno' and IdDisciplina = '$id_disciplina' and IdFaseNota = '$id_fase'");
+	$res2 = $query2->fetchAll(PDO::FETCH_ASSOC);
 
 
+	$notafinal = $res2[0]['NotaFase'];
+	$quantFaltas = $res2[0]['Faltas'];
+
+	if (empty($res_ver)) {
+
+		
+
+		$pdo->query("INSERT INTO tbhistoriconotas SET CodigoSerie = '$codigo_serie', IdAluno = '$aluno', IdDisciplina = '$id_disciplina', NotaFinal = '$notafinal', CargaHorariaAnual = '$carga_horaria_disciplina', QuantidadeFaltasAnual = '$quantFaltas', ResultadoFinal = '$situacao_disc' , AnoConclusao = '$ano'");
 
 
+		
+	}else{
 
-	$res->execute();
+		$pdo->query("UPDATE tbhistoriconotas SET NotaFinal = '$notafinal', CargaHorariaAnual = '$carga_horaria_disciplina', QuantidadeFaltasAnual = '$quantFaltas', ResultadoFinal = '$situacao_disc'  where CodigoSerie = '$codigo_serie' and IdAluno = '$aluno' and IdDisciplina = '$id_disciplina' and AnoConclusao = '$ano'");
+
+		
+	}
+
+	
 
 
 }
-
-
 
 
 
